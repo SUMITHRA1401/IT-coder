e86f4c698a532c25b83adbfd66b542e437a19c29
SUMITHRA1401 <48052910+SUMITHRA1401@users.noreply.github.com>
2019-03-12 12:40:04 +0530

    Create leapyr.c

---
+++ leapyr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter the year:");
+    scanf("%d",&n);
+    if(n%4==0)
+        printf("it is leap year");
+    else
+        printf("it is not a leap year");
+    
+}

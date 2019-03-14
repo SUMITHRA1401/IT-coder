5c5626b345178989cbf4510d0814f972c88dd0f0
SUMITHRA1401 <48052910+SUMITHRA1401@users.noreply.github.com>
2019-03-14 17:59:39 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n,flag=0;
+    printf("Enter the integer value:");
+    scanf("%d",&n);
+
+    for(i=2;i<=n/2;++i)
+    {
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+
+    if (n==1) 
+    {
+      printf("1 is neither a prime nor a composite number.");
+    }
+    else 
+    {
+        if (flag==0)
+          printf("%d yes prime number");
+        else
+          printf("%d no not a prime number");
+    }
+    
+    return 0;
+}
